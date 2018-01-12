a4c4cd556e648d643f7e1622d6e744d409cfb47c
stufinn <30563561+finst@users.noreply.github.com>
2018-01-12 09:59:50 -0600

    Add snippet for JS built-in type properties and methods

---
+++ 18-01-12_BuiltInTypeMethods.js
@@ -0,0 +1,10 @@
+//Code snippet for Built-In Type Methods, page 34-35 Up & Going
+
+var a = "hello world";
+var b = 3.14159;
+
+a.length;			//built in property - displays a characteristic.  i.e. 11 characters long
+a.toUpperCase();	//built-in method - changes to allcaps
+a.toFixed(4);		//built-in method - outputs STRING of #'s rounded to 4 decimal places' -> "3.1416"
+
+//See notes from Jan 12 for discussion of object wrappers.  Need to review this concept again later.
